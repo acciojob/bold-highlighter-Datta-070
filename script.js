b433d57ffe1d1d964b580b22deb0422227f8392a
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-02-12 14:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
+	const strong=document.querySelectorAll("strong");
+	strong.forEach(str=>{
+		str.style.color=("green");
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	const normal=document.querySelectorAll("strong");
+    normal.forEach(nml=>{
+		nml.style.color=("black");
+	});
 }
